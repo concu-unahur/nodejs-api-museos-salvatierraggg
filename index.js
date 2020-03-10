792379857f4cc25ab7cae94cfe7d86406d44023d
salvatierraggg <salvatierraggg@gmail.com>
2020-03-10 20:20:28 -0300

    no entiendo que sale mal

--- index.js
+++ index.js
@@ -5,18 +5,34 @@ function imprimirMuseos(error, respuesta) {
     throw new Error('algo se rompió', error);
   }
 
+  var contenido=[];
   const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
 
+  for (i=0;i<cantidad;i++)
+   contenido.append(`${museos[i].nombre} ${museos[i].direccion}    Por cualquier consulta comunicarse al TELEFONO: ${museos[i].telefono} \n`);  
+
   console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  console.log(`El primer museo se llama ${museos[0]}.`)
 }
+
+
+function controlarErrores(error){
+  if (error) {
+    throw new Error('salio error', error);
+  }
+  console.log("todo bien")
+}
+
+
+
 
 console.log('Antes de llamar a superagent')
 
 superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
-  .query({ format: 'json' })
-  .end(imprimirMuseos)
+.get('https://www.cultura.gob.ar/api/v2.0/museos')
+.query({ format: 'json' })
+.end(imprimirMuseos)
 
 console.log('Después de llamar a superagent')
+
